f9c01321d421e01078a44646130857c5948d3169
Derrek Harrison <derrek.s.harrison@gmail.com>
2018-03-05 12:57:55 -0800

    Add testing for IO with cleanup

--- deck_test.go
+++ deck_test.go
@@ -1,12 +1,38 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
-	exp := 52
 
-	if len(d) != exp {
-		t.Errorf("Expected deck length of %v, got %v", exp, len(d))
+	if len(d) != 52 {
+		t.Errorf("Expected deck length of 52, got %v", len(d))
 	}
+
+	if d[0] != "Two of Spades" {
+		t.Errorf("Expected first card of Two of Spaces, got %v", d[0])
+	}
+
+	if d[len(d)-1] != "Ace of Diamonds" {
+		t.Errorf("Expected last card of Ace of Spades, got %v", d[len(d)-1])
+	}
+}
+
+func TestSaveToDeckAndNewDeckTestTestFromFile(t *testing.T) {
+	os.Remove("_deckTesting")
+
+	deck := newDeck()
+
+	deck.saveToFile("_deckTesting")
+
+	loadedDeck := newDeckFromFile("_deckTesting")
+
+	if len(loadedDeck) != 52 {
+		t.Errorf("Expected deck length of 52, got %v", len(loadedDeck))
+	}
+
+	os.Remove("_deckTesting")
 }
